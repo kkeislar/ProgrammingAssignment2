8eba53b03bfe3d62c53706bee3ea671d0541d60f
Kanti <kkeislar@gmail.com>
2016-05-08 15:40:33 -0500

    message

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## JHU R Prog Week 2
+## Cache Matrix Assignment
+## Kanti Keislar
 
 ## Creates a set of functions to set and get a matrix 'x' and its inverse
 
